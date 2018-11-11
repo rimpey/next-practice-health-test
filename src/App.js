d514ecabaf9b8486349240996bec841060b22da7
Rhonda Impey <rhonda.impey@ping-works.com.au>
2018-11-11 19:46:28 +1100

    Application Build for Next Practice Health take home test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import registrationDefinition from './registrationDefinition.json';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form>
+        {registrationDefinition.map((field, index) =>
+            <label key={index}>
+              {field.title}
+              {(() => {
+                switch(field.type) {
+                  case 'text':
+                    return <input type="text" name={field.name} />;
+                  case 'date':
+                    return <input type="date" name={field.name} />;
+                  case 'select':
+                    return <select>
+                        <option value="">Select</option>
+                        {field.options.map((choice, index) =>
+                          <option key={index} value={choice.value}>{choice.display}</option>
+                        )}
+                    </select>;
+                  case 'multiple':
+                    return <div>
+                      {field.contacts.map((item, index) =>
+                        <label key={index}>
+                          {item.display}
+                          <input type={field.fieldType} name={item.name} />
+                        </label>
+                      )}
+                    </div>;
+                  case 'checkbox':
+                    return <input type="checkbox" value={field.name} name={field.title} />;
+                  default:
+                    return null;
+                }
+              })()}
+
+            </label>
+          )}
+      </form>
     );
   }
 }
